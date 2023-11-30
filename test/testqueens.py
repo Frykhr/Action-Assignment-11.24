b84e2f220b074028c43ba536d74dcba97d8367fc
IVseIce <z1329474143@163.com>
2023-11-30 20:19:02 +0800

    test

---
+++ test/testqueens.py
@@ -0,0 +1,6 @@
+from src.eight_queens import solve_queens
+
+def test_queens():
+    assert solve_queens(8, 4, 3) == 12
+    # assert eight_queens.solve_queens(8, 5, 5) == 2
+    # assert eight_queens.solve_queens(8, 1, 1) == 1
