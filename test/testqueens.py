4d579a5cbac4262e1e08f41340cbed1527961876
IVseIce <z1329474143@163.com>
2023-11-30 20:33:04 +0800

    test

--- test/testqueens.py
+++ test/testqueens.py
@@ -5,7 +5,10 @@ class TestEightQueens(unittest.TestCase):
     
     def test_queens_valid(self):
         # 测试 solve_queens 函数是否返回正确的解决方案数量
-        self.assertEqual(solve_queens(8, 4, 3), 12, "Should be 12 solutions")
+        res = self.assertEqual(solve_queens(8, 4, 3), 12)
+        res2 = solve_queens(8, 4, 3)
+        print(res)
+        print(res2)
 
 if __name__ == '__main__':
     unittest.main()
